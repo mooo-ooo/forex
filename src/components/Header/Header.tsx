f198d2fc24c0188d79f5da9b28fde09cce37b756
huquduy <huquduy@gmail.com>
2023-06-03 22:32:55 +0700

    feat: fonts added

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,18 +2,30 @@ import styled from "styled-components"
 
 function Header() {
   return (
-    <header>
+    <HeaderStyled>
       <h2>
         My Header
       </h2>
-    </header>
+    </HeaderStyled>
   );
 }
 
-const HeaderStyled = styled(Header)`
+const HeaderStyled = styled.header`
   position: fixed;
   top: 0;
   width: 100%;
+  display: flex;
+  -webkit-box-pack: justify;
+  justify-content: space-between;
+  -webkit-box-align: center;
+  align-items: center;
+  width: 100%;
+  height: 56px;
+  background-color: #27262c;
+  border-bottom: 1px solid #383241;
+  transform: translate3d(0px, 0px, 0px);
+  padding-left: 16px;
+  padding-right: 16px;
 `
 
-export default HeaderStyled
+export default Header
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,15 +1,24 @@
 import React from "react";
 import Header from 'components/Header'
+import styled from "styled-components"
 
 const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
-    <>
+    <LayoutStyled>
       <Header />
-      <div>
+      <BodyStyled>
         {children}
-      </div>
-    </>
+      </BodyStyled>
+    </LayoutStyled>
   );
 }
+
+const LayoutStyled = styled.div`
+  background: radial-gradient(103.12%50%at 50%50%,#21193a 0%,#191326 100%);
+`
+
+const BodyStyled = styled.div`
+  min-height: 100vh
+`
 
 export default Layout
